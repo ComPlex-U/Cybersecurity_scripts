8078494fb3ce55d76afb0249910c28c12ec48cdd
johngstephen <jonhgstephen@gmail.com>
2021-03-23 21:33:58 +0000

    scan open ip port with input site

--- URL_Parse.py
+++ URL_Parse.py
@@ -1,16 +1,50 @@
-from urllib.parse import urlparse
 import socket
+import time
+import threading
+
+from queue import Queue
+
 qurl="www.facebook.com"
 url = input('Enter the url site to check: ')
-#o = urlparse('http://www.cwi.nl:80/%7Eguido/Python.html')
-o = urlparse(url)
-o
-print(o)
-
-o.scheme
-print(o.scheme)
-o.port
-print(o.port)
-o.geturl()
-print(o.geturl())
 print("IP:",socket.gethostbyname(url))
+result=socket.gethostbyname(url)
+
+socket.setdefaulttimeout(0.25)
+print_lock = threading.Lock()
+
+target = result
+t_IP = socket.gethostbyname(target)
+print('Starting scan on host: ', t_IP)
+
+
+def portscan(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        con = s.connect((t_IP, port))
+        with print_lock:
+            print(port, 'is open')
+        con.close()
+    except:
+        pass
+
+
+def threader():
+    while True:
+        worker = q.get()
+        portscan(worker)
+        q.task_done()
+
+
+q = Queue()
+startTime = time.time()
+
+for x in range(100):
+    t = threading.Thread(target=threader)
+    t.daemon = True
+    t.start()
+
+for worker in range(1, 8080):
+    q.put(worker)
+
+q.join()
+print('Time taken:', time.time() - startTime)
